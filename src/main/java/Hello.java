422e4933a0484a2cb4b29a46915a6cc826509529
Javiddarvish <javid7darvish@gmail.com>
2021-01-26 15:44:34 -0500

    master watching in living room

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,5 +6,5 @@ public class Hello {
         // movie on the phone 20 min
         // final hour 10 min before going back
     }
-        // i liked this movie who played it?
+        // i liked this movie who played it? lets continue
 }
